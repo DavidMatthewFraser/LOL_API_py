410e7af491a06657db9d2a7d795194d895377ed2
Derek Barker <Dsbarker@mymail.mines.edu>
2020-03-20 17:26:03 -0600

    Added the variables to catch the json info for sums

--- Python_Scripts/read_data_summoner.py
+++ Python_Scripts/read_data_summoner.py
@@ -1,15 +1,21 @@
 import requests
 
 def main():
-    print("Enter your region (NA1 EUW EUNE LAN BR KR LAS OCE TR RU PBE) to get started\n")
+    print("Enter your region (NA1 EUW1 EUN1 LA1 LA2 JP1 OC1 BR1 KR RU) to get started")
     region = input()
-    print("Now, enter your summoners name without spaces /n")
+    print("Now, enter your summoners name without spaces")
     summonerName = input()
     print("Copy and paste your API Key here: /n")
     APIKey = input()
     #Gets the necessary input to get information from the lol API
     responseFile = requestData(region, summonerName, APIKey)
-    print(responseFile)
+    ID = responseFile['id']
+    accountID = responseFile['accountId']
+    puuid = responseFile['puuid']
+    name = responseFile['name']
+    profileIcon = responseFile['profileIconId']
+    revisionDate = responseFile['revisionDate']
+    summonerLevel = responseFile['summonerLevel']
     
 def requestData(region, summonerName, APIKey):
     API = "/?api_key="
