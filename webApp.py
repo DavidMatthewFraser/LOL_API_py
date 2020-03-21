b63ffca9fb8d129c2a10f0274b5ac0252d901c3c
David Fraser <davidfraser@mail.com>
2020-03-20 19:00:51 -0600

    added changesto webApp.py  and profile.html

--- webApp.py
+++ webApp.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from read_data_summoner import *
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def hello_world():
 @app.route('/profile')
 @app.route('/profile/<name>')
 def hello(name=None):
+    name = readSummoner('NA1', name, 'RGAPI-fb4acaba-bdef-451b-9ceb-33b234d0bb6d')['Name'];
+    profileIcon = readSummoner('NA1', name, 'RGAPI-fb4acaba-bdef-451b-9ceb-33b234d0bb6d')['profileIcon'];
+    summonerLevel = readSummoner('NA1', name, 'RGAPI-fb4acaba-bdef-451b-9ceb-33b234d0bb6d')['summonerLevel'];
+
     if(name != None):
         name = name + "'s profile'"
-    return render_template('profile.html', name=name, random="hello")
+    return render_template('profile.html', name=name, profileIcon=profileIcon, summonerLevel=summonerLevel)
