2db3c51d62ade1ffa2bb8df1d4f471bf88246805
David Fraser <davidfraser@mail.com>
2020-03-23 13:31:37 -0600

    added html/js so that a user can enter name and api_key and routes to profile page with that info

--- webApp.py
+++ webApp.py
@@ -2,20 +2,16 @@ from flask import Flask
 from flask import render_template
 from read_data_summoner import *
 
-api_key = "" # your stream key
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 @app.route('/profile')
-@app.route('/profile/<name>')
-def hello(name=None):
+@app.route('/profile/<name>/<api_key>')
+def hello(name=None,api_key=None):
     name = readSummoner('NA1', name, api_key)['Name']
     profileIcon = str(readSummoner('NA1', name, api_key)['profileIcon'])
     summonerLevel = readSummoner('NA1', name, api_key)['summonerLevel']
 
-    if(name != None):
-        name = name + "'s profile'"
     return render_template('profile.html', sum_name=name, profileIcon=profileIcon, summonerLevel=summonerLevel)
